e65d6179fbc822b468e4e3a22d1832bbca1e6c22
rrrichard <richard.rabulan@hotmail.co.uk>
2019-04-25 23:42:43 +0100

    added countdown and a game over message when the time has run out

--- index.js
+++ index.js
@@ -9,10 +9,33 @@ window.addEventListener('load', init);
 function init(){
     console.log(init)
     showWord(words);
+    setInterval(countdown, 1000);
+    setInterval(checkStatus, 50);
 }
 
 function showWord(words){
     //fix this bit
+    let currentWord = document.querySelector('#current-word')
     let randIndex = Math.floor(Math.random() * words.length);
-    console.log(randIndex);
+
+    currentWord.innerHTML = words[randIndex];
+}
+
+function countdown(){
+
+    let timeDisplay = document.querySelector('#time')
+    if (time > 0){
+        time--;
+    } else if (time === 0){
+        isPlaying = false;
+    }
+    timeDisplay.innerHTML = time;
+}
+
+function checkStatus(){
+
+    let message = document.querySelector('#message')
+    if (!isPlaying && time === 0){
+        message.innerHTML = 'Game Over!';
+    }
 }
